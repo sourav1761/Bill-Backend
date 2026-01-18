8dd43c40aa330018af095dd40f2533867ea2504f
sourav1761 <souravpatidar55@gmail.com>
2026-01-18 20:09:14 +0530

    ....

--- src/config/db.js
+++ src/config/db.js
@@ -2,8 +2,12 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGODB_URI);
+const conn = await mongoose.connect(process.env.MONGODB_URI, {
+  dbName: "rajshree_collection",
+});
     console.log(`✅ MongoDB Connected: ${conn.connection.host}`);
+    console.log("✅ Connected DB:", conn.connection.name);
+
   } catch (error) {
     console.error(`❌ MongoDB Connection Error: ${error.message}`);
     process.exit(1);
